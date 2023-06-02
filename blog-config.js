b484056f61184b1ceacb9e555374179cb008a1c2
jemin <jemin03120111@gmail.com>
2023-06-02 23:15:33 +0900

    .

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "jemlog",
+  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
+  author: "jemin",
+  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  links: {
+    github: "https://github.com/jemlog",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    instagram: "https://www.instagram.com",
+    email: "jemin03120111@gmail.com",
+  },
+  utterances: {
+    repo: "jemlog/jemlog.github.io",
+    type: "pathname",
+  },
+}
